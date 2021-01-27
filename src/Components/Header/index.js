7ba1cb5e191ee86fdb2aa83f455c610fd92d697e
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2021-01-27 15:39:48 -0500

    basic header skeleton.

---
+++ src/Components/Header/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import NavBar from "../NavBar/index";
+
+export default function Header() {
+    return (
+        <section className="header">
+
+            <section className="header-top">
+                <section className="header-top-about">
+                    <a href="/" className="header-about">About Me</a>
+                </section>
+
+                <section className="header-top-NavBar">
+                    <NavBar />
+                </section>
+            </section>
+
+            <section className="header-bottom">
+                <section className="header-bottom-github">
+                    <a href="https://github.com/DevDan13" alt="DevDan13">DevDan13</a>
+                </section>
+
+                <section className="header-bottom-resume">
+                    {/* pdf resume link here */}
+                </section>
+
+                <section className="header-bottom-email">
+                    dan1397732@gmail.com
+                </section>
+            </section>
+
+        </section>
+
+    )
+}
