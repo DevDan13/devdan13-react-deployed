1b4895622441b4ab571e8dc641a3c028c1440808
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2021-02-02 18:10:11 -0500

    fixed a tag instead of Link issue.

--- src/Components/CardItem/index.js
+++ src/Components/CardItem/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function CardItem(props) {
   return (
     <>
       <li className='card-item'>
-        <Link className='card-item-link' target="_blank" to={{ pathname: props.path}} >
+        <a className='card-item-link' target="_blank" href={props.path} rel="noreferrer">
           <figure className='card-item-pic-wrap' data-category={props.label}>
             <img
               className='card-item-img'
@@ -16,7 +15,7 @@ function CardItem(props) {
           <div className='card-item-info'>
             <h5 className='card-item-text'>{props.text}</h5>
           </div>
-        </Link>
+        </a>
       </li>
     </>
   );
